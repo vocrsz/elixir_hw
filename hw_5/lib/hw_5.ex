29c1557776752969c872f94873369a48a6d1f398
Nikita Kononov <vocrsz@gmail.com>
2024-11-19 18:23:17 +0100

    [hw_5] phoenix init project

---
+++ hw_5/lib/hw_5.ex
@@ -0,0 +1,9 @@
+defmodule Hw5 do
+  @moduledoc """
+  Hw5 keeps the contexts that define your domain
+  and business logic.
+
+  Contexts are also responsible for managing your data, regardless
+  if it comes from the database, an external API or others.
+  """
+end
---
+++ hw_5/lib/hw_5/mailer.ex
@@ -0,0 +1,3 @@
+defmodule Hw5.Mailer do
+  use Swoosh.Mailer, otp_app: :hw_5
+end
