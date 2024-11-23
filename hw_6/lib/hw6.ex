4954fe28f8d2cfeb872ededc1dd146d4a345085c
Nikita Kononov <vocrsz@gmail.com>
2024-11-23 13:37:02 +0100

    Init project with deps

---
+++ hw_6/lib/hw6.ex
@@ -0,0 +1,18 @@
+defmodule Hw6 do
+  @moduledoc """
+  Documentation for `Hw6`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Hw6.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ hw_6/test/hw6_test.exs
@@ -0,0 +1,8 @@
+defmodule Hw6Test do
+  use ExUnit.Case
+  doctest Hw6
+
+  test "greets the world" do
+    assert Hw6.hello() == :world
+  end
+end
